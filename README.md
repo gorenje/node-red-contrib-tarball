4c20250bd4259816bc64d97702057883c335c75c
Gerrit Riessen <gerrit@openmindmap.org>
2023-11-23 16:28:38 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-## TarBall for Node-RED
+# Tarball
 
-Work in progress
+Create and extract tar archive files.
+
+### Artifacts
+
+- [Flow maintaining this code](https://flowhub.org/f/cd8ee0cc76ab3339)
+- [GitHub repo](https://github.com/gorenje/node-red-contrib-tarball)
+- [NPMjs package](https://www.npmjs.com/package/@gregoriusrippenstein/node-red-contrib-tarball)
+- [Node-RED package page](https://flows.nodered.org/node/@gregoriusrippenstein/node-red-contrib-tarball)
+
---
+++ nodes/tarball.js
@@ -0,0 +1,186 @@
+module.exports = function(RED) {
+  function CoretarballFunctionality(config) {
+    RED.nodes.createNode(this,config);
+
+    var node = this;
+    var cfg = config;
+
+    const tarStream = require('tar-stream');
+    const streamx = require('streamx');
+    const pakoGzip = require('pako');
+    const lzmaNative = require('lzma-native');
+
+    /*
+     * Extract tar files.
+     */
+    var extractTarFile = (tarData, onFile, onFinish, onError) => {
+      const extract = tarStream.extract()
+
+      var allFiles = [];
+
+      extract.on('entry', function (header, stream, next) {
+        // header is the tar header
+        // stream is the content body (might be an empty stream)
+        // call next when you are done with this entry
+
+        var buffer = [];
+
+        stream.on('data', function (data) {
+          buffer.push(data)
+        });
+
+        stream.on('end', function () {
+          //@ts-ignore
+          onFile(header.name, Buffer.concat(buffer))
+
+          allFiles.push({
+            path: header.name,
+            payload: Buffer.concat(buffer)
+          })
+
+          next() // ready for next entry
+        })
+
+        stream.resume() // just auto drain the stream
+      })
+
+      extract.on('finish', function () {
+        onFinish(allFiles)
+      })
+
+      extract.on('error', onError);
+
+      var stream = streamx.Readable.from(Buffer.from(tarData))
+      stream.pipe(extract);
+    }
+
+    var extract = (payload, onFile, onDone, onTarError, onOtherError) => {
+      import('file-type').then(module => {
+        const buffer = new Uint8Array(payload);
+
+        module.fileTypeFromBuffer(buffer).then(d => {
+          switch (d.ext) {
+            case 'gz':
+              extractTarFile(pakoGzip.inflate(buffer), onFile, onDone, onTarError)
+              break
+            case 'tar':
+              extractTarFile(buffer, onFile, onDone, onTarError)
+              break
+            case 'xz':
+              lzmaNative.decompress(Buffer.from(buffer)).then(data => {
+                extractTarFile(data, onFile, onDone, onTarError)
+              }).catch(err => {
+                onOtherError("tarball.error.xzcorrupt", err)
+              })
+              break
+            default:
+              onOtherError("tarball.error.formatnotsupported", err)
+          }
+        }).catch(err => {
+          onOtherError("tarball.error.general", err)
+        })
+      }).catch(err => {
+        onOtherError("tarball.error.general", err)
+      })
+    }
+
+    var compress = (payload, onDone, onError) => {
+      const pack = tarStream.pack()
+
+      var buffer = [];
+
+      pack.on('end', function () {
+        onDone(Buffer.concat(buffer))
+      });
+
+      pack.on('data', function (data) {
+        buffer.push(data)
+      });
+
+      pack.on('error', (err) => {
+        onError(err)
+      })
+
+      payload.forEach((elem) => {
+        if ( elem.path && elem.path != "" ) {
+          pack.entry({ name: elem.path }, elem.payload)
+        }
+      })
+
+      pack.finalize()       
+    }
+
+    /*
+     * Close something
+     */
+    node.on('close', function() {
+      node.status({});
+    });
+
+    /* 
+      * msg handler, in this case pass the message on unchanged 
+      */
+    node.on("input", function(msg, send, done) {
+
+      /* no payload, no dice */
+      if (!msg.hasOwnProperty('payload'))
+        return node.send(msg);
+
+      /** Extraction handlers **/
+      var onFile = (path, content) => {
+        send({
+          ...msg,
+          path: path,
+          payload: content
+        })
+      };
+
+      var onDone = (allFiles) => {
+        send({
+          ...msg,
+          complete: true,
+          payload: allFiles
+        })
+        done();
+      };
+
+      var onTarError = (err) => {
+        msg.error = err
+        done(RED._("tarball.error.untar"), msg)
+      };
+
+      var onOtherError = (localeKey, err) => {
+        msg.error = err
+        done(RED._(localeKey), msg)
+      };
+      
+      /** compression handlers **/
+      var onCompDone = (tardata) => {
+        msg.payload = tardata
+        send(msg)
+        done()
+      };
+
+      var onCompError = (err) => {
+        msg.error = err
+        done(RED._("tarball.error.compressfailed"), msg)
+      };
+      
+      /** handle the payload */
+      switch (typeof msg.payload ) {
+        case 'object':
+          if (Array.isArray(msg.payload)) {
+            compress( msg.payload, onCompDone, onCompError)
+          } else {
+            extract(msg.payload, onFile, onDone, onTarError, onOtherError)
+          }
+          break;
+        default:
+          done(RED._("tarball.error.unknownpayload"), msg)
+      }
+    });
+  }
+
+  RED.nodes.registerType("tarball", CoretarballFunctionality);
+
+}
